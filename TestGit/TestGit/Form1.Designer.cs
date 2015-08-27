59d96545afed4cab7a76695e3424a093ccadf049
JohnTayback <david.tissot.74@gmail.com>
2015-08-27 10:58:26 +0200

    Liaison form1/form2

--- TestGit/TestGit/Form1.Designer.cs
+++ TestGit/TestGit/Form1.Designer.cs
@@ -36,12 +36,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 195);
+            this.button1.Location = new System.Drawing.Point(175, 195);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "vers 2";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // monthCalendar1
             // 
--- TestGit/TestGit/Form2.cs
+++ TestGit/TestGit/Form2.cs
@@ -10,21 +10,18 @@ using System.Windows.Forms;
 
 namespace TestGit
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 Form2 = new Form2();
-            Form2.Show();
+            Form1 Form1 = new Form1();
+            Form1.Show();
             this.Hide();
-
         }
-
-      
     }
 }
